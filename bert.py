e8e47c4d8aa26e0761382a7bb4f379ab8e4fddbd
Savitha Srinivasan <savsrin@gmail.com>
2022-06-02 22:57:02 -0700

    Update bert.py

--- bert.py
+++ bert.py
@@ -2,21 +2,16 @@ import csv
 import pandas as pd
 from summarizer import Summarizer
 
+# Code for generating commentary using the BERT Extractive summarizer. 
 df = pd.read_csv('./CONTEXT_b1Val')
 # print(df)
 texts = df['Text']
 summaries = []
 
 for text in texts: 
-    # print("in for loop")
-    # print("text: ", text)
     model = Summarizer() 
     result = model(text, min_length = 2, ratio=0.2)
-    #full = ''.join(result)
-    # print(result)
     summaries.append(result)
-    # print(summaries)
-    # print("end of for loop")
 
 results = pd.DataFrame({'Text': texts, 'Summary': summaries, 'ISBN': df['ISBN'], 'Page': df['Page'] })
 results.to_csv('summaries_contextb1Val.csv', index=False)
